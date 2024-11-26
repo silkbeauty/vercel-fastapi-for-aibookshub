b3d258c50adc58bcfa4bf6b55f50d41dc6b0718a
aiaccfin <aiaccfin@gmail.com>
2024-11-25 19:26:26 -0500

    update

---
+++ app.py
@@ -0,0 +1,16 @@
+#app.py
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def hello_world():
+    return "Hello,World"
+
+
+if __name__ == '__main__':
+    import uvicorn
+    # uvicorn.run(app)
+    uvicorn.run(app, port=8080)
