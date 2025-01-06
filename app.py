649f52a6b19aa81a134725d5ab4223b95249db80
aiaccfin <aiaccfin@gmail.com>
2025-01-05 20:49:50 -0500

    update

--- app.py
+++ app.py
@@ -7,14 +7,6 @@ from apps.api import router
 
 app = FastAPI()
 
-origins = ["*"]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 app.include_router(router)
 
 
