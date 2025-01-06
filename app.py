7fd78aa19c9d5d7dfb7667cff9bac265985ae5a6
aiaccfin <aiaccfin@gmail.com>
2025-01-05 20:47:25 -0500

    update

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from fastapi import FastAPI
 import uvicorn
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from apps.api import router 
 
 app = FastAPI()
 
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(router)
 
-@app.get('/')
-def _():    return "Hello,uvicorn Worlddd"
 
 
 if __name__ == '__main__':
--- apps/api/v1/r_root.py
+++ apps/api/v1/r_root.py
@@ -2,5 +2,11 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+@router.get('/')
+def _():    return "Hello, Worlddd"
+
 @router.get('/test')
 async def root(): return {"AIBooksHub": "New View Step by Step a, FastAPI!"}
+
+
+
