fc2d220f8458f3a20f0aff397c72a73bec940b0c
aiaccfin <aiaccfin@gmail.com>
2025-01-05 20:18:36 -0500

    update

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-# from fastapi import FastAPI
+from fastapi import FastAPI
 import uvicorn
-from apps.main import create_app1
 
-# app = FastAPI()
+app = FastAPI()
 
-app = create_app1()
 
 @app.get('/')
-def _():    return "Hello, settings.uvicorn World1"
+def _():    return "Hello,uvicorn Worlddd"
 
 
 if __name__ == '__main__':
+    # uvicorn.run(app, port=8080)
     uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=True)
